30caf0e65e03e94d4cda2d7205c1ba2d40a6fccf
fzhu3396 <fzhu3396@uni.sydney.edu.au>
2022-05-07 22:13:47 +1000

    test4

--- 5347A2/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/userPage', function(reg, res, next) {
-  res.render('userPage', {});
-});
-
-module.exports = router;
