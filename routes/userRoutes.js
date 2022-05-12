1432b3aa12f634b0d3429d2fd50aac48767b1ea0
fzhu3396 <fzhu3396@uni.sydney.edu.au>
2022-05-12 21:07:45 +1000

    Update userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,27 +1,21 @@
-const express = require("express");
-const controller = require("../controllers/userPageController");
+const express = require('express');
+const controller = require('../controllers/userPageController');
 
 const router = express.Router();
 
-router.get("/userPage", function (req, res, next) {
-  res.render("userPage", {
-    user: req.session.user,
-  });
-});
-
-router.get("/userPage/getUserId", controller.getUserId);
-router.get("/userPage/getUserInfo/:id", controller.getUserInfo);
-router.post("/userPage/updateUserInfo", controller.updateUserInfo);
-router.post("/userPage/checkPwd", controller.checkPwd);
-router.post("/userPage/userInfo/pwd", controller.changePassword);
-router.post("/userPage/userInfo/newlisting", controller.addNewListing);
-router.get("/userPage/getUserStatus", controller.getUserStatus);
-router.get("/userPage/changeImageRoutes/:info", controller.changeImageRoutes);
-router.get("/userPage/phoneListings/:id", controller.getRelatedPhoneListings);
-router.post("/userPage/deletePhoneListings", controller.deletePhoneListings);
-router.post("/userPage/disablePhoneListings", controller.disablePhoneListings);
+router.get('/userPage/getUserId', controller.getUserId);
+router.get('/userPage/getUserInfo/:id', controller.getUserInfo);
+router.post('/userPage/updateUserInfo', controller.updateUserInfo);
+router.post('/userPage/checkPwd', controller.checkPwd);
+router.post('/userPage/userInfo/pwd', controller.changePassword);
+router.post('/userPage/userInfo/newlisting', controller.addNewListing);
+router.get('/userPage/getUserStatus', controller.getUserStatus);
+router.get('/userPage/changeImageRoutes/:info', controller.changeImageRoutes);
+router.get('/userPage/phoneListings/:id', controller.getRelatedPhoneListings);
+router.post('/userPage/deletePhoneListings', controller.deletePhoneListings);
+router.post('/userPage/disablePhoneListings', controller.disablePhoneListings);
 router.post(
-  "/userPage/notDisablePhoneListings",
+  '/userPage/notDisablePhoneListings',
   controller.notDisablePhoneListings
 );
 
