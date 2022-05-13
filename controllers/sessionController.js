e82b742dd51050638cc89ef9458cd719ad5ec800
Your Name <you@example.com>
2022-05-13 10:15:56 +0800

    modify session code =

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -1,15 +1,15 @@
-var fs =require('fs');
+var fs = require('fs');
 
-var express =require('express');
+var express = require('express');
 // //创建一个路由容器
 // var router =express.Router()
 
-var Users =require('../models/users');
+var Users = require('../models/users');
 
-var md5 =require('md5');
+var md5 = require('md5');
 
-var nodemailer =require('nodemailer');
-var transporter =nodemailer.createTransport({
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
 	host:'smtp.qq.com',
 	secureConnection:true,
 	port:465,
@@ -20,7 +20,7 @@ var transporter =nodemailer.createTransport({
 	}
 });
 
-module.exports={
+module.exports ={
 	findIndex:function(req,res){
 	
 		Users.find(function(err,users){
@@ -57,9 +57,9 @@ module.exports={
 	},
 	
 	login:function(req, res) {
-		var loginEmail=req.body.email;
-		var loginPass=md5(req.body.password);
-		req.session.isLogin= true;
+		var loginEmail = req.body.email;
+		var loginPass = md5(req.body.password);
+		req.session.isLogin = true;
 		Users.findOne({
 			email: loginEmail,
 			password: loginPass
@@ -91,7 +91,7 @@ module.exports={
 	
 	logout:function(req, res) {
 		//清除登陆状态，重定向到登录页
-		req.session.user=null;
+		req.session.user = null;
 		res.redirect('/');
 	},
 	
@@ -111,7 +111,7 @@ module.exports={
 	},
 	
 	register:function(req, res) {
-		var body=req.body;
+		var body = req.body;
 		Users.findOne({
 			email:body.email
 		},function(err,data){
@@ -128,7 +128,7 @@ module.exports={
 					message: 'Email already exists'
 				});
 			}
-			body.password='NV_'+md5(body.password);
+			body.password = 'NV_'+md5(body.password);
 			new Users(req.body).save(function(err,user){
 				if(err){
 					return res.status(500).json({
@@ -137,14 +137,14 @@ module.exports={
 					});
 				}
 				
-				req.session.user= user;
+				req.session.user = user;
 				
 				res.status(200).json({
 					err_code: 0,
 					message: 'ok'
 				});
 				//验证邮箱
-				var mailOptions={
+				var mailOptions ={
 					from: '1263558065@qq.com',
 					to: body.email,
 					subject: 'vertify email address',
@@ -165,9 +165,9 @@ module.exports={
 	},
 	
 	activate:function(req, res) {
-		var activateEmail=req.query.email;
-		var activatePass=req.query.password;
-		activatePass=activatePass.substring(3);
+		var activateEmail = req.query.email;
+		var activatePass = req.query.password;
+		activatePass = activatePass.substring(3);
 		// console.log(activatePass)
 		Users.updateOne({email: activateEmail},{$set:{password:activatePass}},function(err,data){
 		if(err){
@@ -185,8 +185,8 @@ module.exports={
 	},
 	
 	verification:function(req, res) {
-		var userEmail=req.body;
-		var email=req.body.email;
+		var userEmail = req.body;
+		var email = req.body.email;
 		//查找数据库中是否用户使用这个邮箱
 		Users.find(
 			userEmail
@@ -198,7 +198,7 @@ module.exports={
 				});
 			}else{
 				// console.log(user)
-				if(user.length===0){
+				if(user.length ===0){
 					return res.status(200).json({
 						err_code: 1,
 						message: 'email not exists'
@@ -219,7 +219,7 @@ module.exports={
 	jump:function(req, res) {
 		res.render('jump.html');
 		var userEmail = req.query.email;
-		var mailOptions={
+		var mailOptions ={
 			from: '1263558065@qq.com',
 			to: userEmail,
 			subject: 'change password',
@@ -238,16 +238,16 @@ module.exports={
 	
 	resetPage:function(req, res) {
 		var email = req.query.email;
-		req.session.userEmail= email;
+		req.session.userEmail = email;
 		res.render('reset.html');
 	},
 	
 	reset:function(req, res) {
-		var Cpass=req.body.Cpassword;
-		var newPass=req.body.password;
-		if(Cpass===newPass){
+		var Cpass = req.body.Cpassword;
+		var newPass = req.body.password;
+		if(Cpass ===newPass){
 			var email = req.session.userEmail;
-			newPass=md5(req.body.password);
+			newPass = md5(req.body.password);
 			Users.updateOne({email: email},{$set:{password:newPass}},function(err,data){
 			if(err){
 			  return res.status(500).json({
